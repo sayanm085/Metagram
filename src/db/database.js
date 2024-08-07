61cf0a2949c9cab709715a024aec8f6922eacc75
Sayan Mondal <sayanm085@gmail.com>
2024-08-07 16:58:52 +0530

    new up

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 let databaseconnection = async () => {
     try {
-        await mongoose.connect(`mongodb://localhost:27017/MetaGram`)
+        await mongoose.connect(`mongodb+srv://Sayanm0981:Sayanm0912@pikatube.ugxdp.mongodb.net/?retryWrites=true&w=majority&appName=pikatube`)
         .then(() => { console.log("Database connected") })
         .catch((err) => 
         { console.log(err) });
