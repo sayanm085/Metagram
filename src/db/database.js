f9213b3f11be5ff561a390f2840ed3e9e2cfc14e
Sayan Mondal <154863207+sayanm085@users.noreply.github.com>
2024-11-20 19:17:22 +0530

    Update database.js

--- src/db/database.js
+++ src/db/database.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 let databaseconnection = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://Sayanm0981:Sayanm0912@pikatube.ugxdp.mongodb.net/?retryWrites=true&w=majority&appName=pikatube`)
+        await mongoose.connect(`mongodb+srv://Shotlin0912:Shotlin0912@shotlin.jpiyx.mongodb.net/?retryWrites=true&w=majority&appName=pikatube`)
         .then(() => { console.log("Database connected") })
         .catch((err) => 
         { console.log(err) });
